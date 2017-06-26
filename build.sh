cc441d600a264fdfc9aa301e36d8712dbf656fed
kbirk <birk.kevin@gmail.com>
2017-06-26 18:40:27 -0400

    Update final build command. Remove unused local copy of `wait-for-it.sh`

--- build.sh
+++ build.sh
@@ -5,15 +5,18 @@ source ./server/config.sh
 HIGHLIGHT='\033[0;34m'
 NC='\033[0m'
 
+
 echo -e "${HIGHLIGHT}Getting veldt-ingest..${NC}"
 
 # get veldt-ingest and force a static rebuild of it so that it can run on Alpine
 go get -u -v github.com/unchartedsoftware/distil-ingest
-env GOOS=linux GOARCH=amd64 go build github.com/unchartedsoftware/distil-ingest
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -v -a github.com/unchartedsoftware/distil-ingest
 mv distil-ingest ./server
 
-# copy the d3m data into the docker context
+
 echo -e "${HIGHLIGHT}Copying D3M data..${NC}"
+
+# copy the d3m data into the docker context
 mkdir -p ./server/data
 for dataset in "${DATASETS[@]}"
 do
@@ -23,6 +26,8 @@ done
 
 
 echo -e "${HIGHLIGHT}Building image ${DOCKER_IMAGE_NAME}...${NC}"
+
+# build the docker image
 cd server
 docker build -t docker.uncharted.software/$DOCKER_IMAGE_NAME:${DOCKER_IMAGE_VERSION} .
 cd ..
