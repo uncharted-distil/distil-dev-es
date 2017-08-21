31e5272589259c492d491a4c500ddf4a0545b335
kbirk <birk.kevin@gmail.com>
2017-08-21 15:30:52 -0400

    Added s3 upload and kafka-classification step.

--- build.sh
+++ build.sh
@@ -9,10 +9,13 @@ echo -e "${HIGHLIGHT}Getting distil-ingest..${NC}"
 
 # get distil-ingest and force a static rebuild of it so that it can run on Alpine
 go get -u -v github.com/unchartedsoftware/distil-ingest/cmd/distil-merge
+go get -u -v github.com/unchartedsoftware/distil-ingest/cmd/distil-classify
 go get -u -v github.com/unchartedsoftware/distil-ingest/cmd/distil-ingest
 env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a github.com/unchartedsoftware/distil-ingest/cmd/distil-merge
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a github.com/unchartedsoftware/distil-ingest/cmd/distil-classify
 env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a github.com/unchartedsoftware/distil-ingest/cmd/distil-ingest
 mv distil-merge ./server
+mv distil-classify ./server
 mv distil-ingest ./server
 
 echo -e "${HIGHLIGHT}Copying D3M data..${NC}"
@@ -29,6 +32,11 @@ echo -e "${HIGHLIGHT}Building image ${DOCKER_IMAGE_NAME}...${NC}"
 
 # build the docker image
 cd server
-docker build -t docker.uncharted.software/$DOCKER_IMAGE_NAME:${DOCKER_IMAGE_VERSION} .
+
+docker build \
+    --build-arg aws_access_key=$AWS_ACCESS_KEY \
+    --build-arg aws_secret_key=$AWS_SECRET_KEY \
+    --build-arg aws_region=$AWS_REGION  \
+    --tag docker.uncharted.software/$DOCKER_IMAGE_NAME:${DOCKER_IMAGE_VERSION} .
 cd ..
 echo -e "${HIGHLIGHT}Done${NC}"
