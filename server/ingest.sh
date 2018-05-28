82acd2dc0941be4cf28edfdf0f127468edf13b20
Philippe Horne <phorne@uncharted.software>
2018-05-28 07:46:34 -0400

    Updated scripts to handle featurization.

--- server/ingest.sh
+++ server/ingest.sh
@@ -2,12 +2,33 @@
 
 source ./config.sh
 
-SCHEMA_PATH=/datasetDoc.json
-DATA_PATH=/tables/learningData.csv
+SCHEMA=/datasetDoc.json
+OUTPUT_PATH=features/
+DATASET_FOLDER_SUFFIX=_dataset
+HAS_HEADER=1
+FEATURIZE_FUNCTION=fileupload
+REST_ENDPOINT=HTTP://10.108.4.42:5002
+DATA_SERVER=HTTP://10.108.4.104
+
+for DATASET in "${DATASETS[@]}"
+do
+    echo "--------------------------------------------------------------------------------"
+    echo " Featurizing $DATASET dataset"
+    echo "--------------------------------------------------------------------------------"
+    ./distil-featurize \
+        --rest-endpoint="$REST_ENDPOINT" \
+        --featurize-function="$FEATURIZE_FUNCTION" \
+        --dataset="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN" \
+        --media-path="$DATA_SERVER/${DATASET}" \
+        --schema="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$SCHEMA" \
+        --output="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN" \
+        --has-header=$HAS_HEADER
+done
+
+DATA_PATH=/features/features.csv
 MERGED_OUTPUT_PATH=tables/merged.csv
 MERGED_OUTPUT_HEADER_PATH=tables/mergedHeader.csv
-OUTPUT_SCHEMA=tables/mergedDataSchema.json
-DATASET_FOLDER_SUFFIX=_dataset
+OUTPUT_SCHEMA=mergedDatasetDoc.json
 MERGE_HAS_HEADER=1
 
 for DATASET in "${DATASETS[@]}"
@@ -16,16 +37,17 @@ do
     echo " Merging $DATASET dataset"
     echo "--------------------------------------------------------------------------------"
     ./distil-merge \
-        --schema="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$SCHEMA_PATH" \
-        --data="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$DATA_PATH" \
-        --raw-data="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/" \
-        --output-path="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$MERGED_OUTPUT_PATH" \
-        --output-path-header="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$MERGED_OUTPUT_HEADER_PATH" \
-        --output-schema-path="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$OUTPUT_SCHEMA" \
+        --schema="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/featureDatasetDoc.json" \
+        --data="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$DATA_PATH" \
+        --raw-data="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/" \
+        --output-path="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$MERGED_OUTPUT_PATH" \
+        --output-path-relative="$MERGED_OUTPUT_PATH" \
+        --output-path-header="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$MERGED_OUTPUT_HEADER_PATH" \
+        --output-schema-path="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$OUTPUT_SCHEMA" \
         --has-header=$MERGE_HAS_HEADER
 done
 
-CLASSIFICATION_OUTPUT_PATH=tables/classification.json
+CLASSIFICATION_OUTPUT_PATH=classification.json
 REST_ENDPOINT=http://127.0.0.1:5000
 CLASSIFICATION_FUNCTION=fileUpload
 
@@ -37,11 +59,11 @@ do
     ./distil-classify \
         --rest-endpoint="$REST_ENDPOINT" \
         --classification-function="$CLASSIFICATION_FUNCTION" \
-        --dataset="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$MERGED_OUTPUT_PATH" \
-        --output="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$CLASSIFICATION_OUTPUT_PATH"
+        --dataset="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$MERGED_OUTPUT_PATH" \
+        --output="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$CLASSIFICATION_OUTPUT_PATH"
 done
 
-IMPORTANCE_OUTPUT=tables/importance.json
+IMPORTANCE_OUTPUT=importance.json
 RANKING_REST_ENDPOINT=HTTP://127.0.0.1:5001
 RANKING_FUNCTION=pca
 NUMERIC_OUTPUT_SUFFIX=_numeric.csv
@@ -54,19 +76,19 @@ do
     echo " Ranking $DATASET dataset"
     echo "--------------------------------------------------------------------------------"
     ./distil-rank \
-        --schema="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$OUTPUT_SCHEMA" \
-        --dataset="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$MERGED_OUTPUT_PATH" \
+        --schema="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$OUTPUT_SCHEMA" \
+        --dataset="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$MERGED_OUTPUT_PATH" \
         --rest-endpoint="$RANKING_REST_ENDPOINT" \
         --ranking-function="$RANKING_FUNCTION" \
-        --ranking-output="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$DATASET$NUMERIC_OUTPUT_SUFFIX" \
-        --classification="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$CLASSIFICATION_OUTPUT_PATH" \
-        --has-header=$MERGE_HAS_HEADER \
+        --ranking-output="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$DATASET$NUMERIC_OUTPUT_SUFFIX" \
+        --classification="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$CLASSIFICATION_OUTPUT_PATH" \
+        --has-header=$HAS_HEADER \
         --row-limit=$ROW_LIMIT \
-        --output="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$IMPORTANCE_OUTPUT" \
+        --output="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$IMPORTANCE_OUTPUT" \
         --type-source="$TYPE_SOURCE"
 done
 
-SUMMARY_MACHINE_OUTPUT=/tables/summary-machine.json
+SUMMARY_MACHINE_OUTPUT=summary-machine.json
 SUMMARY_REST_ENDPOINT=HTTP://10.108.4.42:5003
 SUMMARY_FUNCTION=fileUpload
 
@@ -78,8 +100,8 @@ do
     ./distil-summary \
         --rest-endpoint="$SUMMARY_REST_ENDPOINT" \
         --summary-function="$SUMMARY_FUNCTION" \
-        --dataset="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$MERGED_OUTPUT_HEADER_PATH" \
-        --output="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$SUMMARY_MACHINE_OUTPUT"
+        --dataset="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$MERGED_OUTPUT_HEADER_PATH" \
+        --output="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$SUMMARY_MACHINE_OUTPUT"
 done
 
 METADATA_INDEX=datasets
@@ -97,12 +119,12 @@ do
         --es-metadata-index="$METADATA_INDEX" \
         --es-data-index="$DATASET" \
         --es-dataset-prefix="d_" \
-        --schema="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$OUTPUT_SCHEMA" \
-        --dataset="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$MERGED_OUTPUT_PATH" \
-        --classification="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$CLASSIFICATION_OUTPUT_PATH" \
-        --summary="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$SUMMARY_OUTPUT_PATH" \
-        --summary-machine="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$SUMMARY_MACHINE_OUTPUT" \
-        --importance="$CONTAINER_DATA_DIR/${DATASET}/${DATASET}$DATASET_FOLDER_SUFFIX/$IMPORTANCE_OUTPUT" \
+        --schema="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$OUTPUT_SCHEMA" \
+        --dataset="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$MERGED_OUTPUT_PATH" \
+        --classification="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$CLASSIFICATION_OUTPUT_PATH" \
+        --summary="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$SUMMARY_OUTPUT_PATH" \
+        --summary-machine="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$SUMMARY_MACHINE_OUTPUT" \
+        --importance="$CONTAINER_DATA_DIR/${DATASET}/TRAIN/dataset_TRAIN/$IMPORTANCE_OUTPUT" \
         --type-source="$TYPE_SOURCE" \
         --clear-existing
 done
