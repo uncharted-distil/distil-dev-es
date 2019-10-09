7b95052dac173a007feff805bb6f56c709f2a1e4
philippe horne <phorne@uncharted.software>
2019-10-09 10:21:43 -0400

    Switched to auto ml from runner.

--- push.sh
+++ push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 source ./server/config.sh
-docker login $DOCKER_REPO
 docker push $DOCKER_REPO/distil_dev_es:latest
 docker push $DOCKER_REPO/distil_dev_es:${DOCKER_IMAGE_VERSION}
-docker logout $DOCKER_REPO
